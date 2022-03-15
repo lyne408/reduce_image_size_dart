93e8c4c7ccb961d13ea1f6cdf7a426f4f7e350c2
lyne408 <lyne408@outlook.com>
2022-03-15 13:32:51 +0800

    directory tree

--- scripts/build_main.dart
+++ scripts/build_main.dart
@@ -4,7 +4,7 @@ import 'package:path/path.dart' as path;
 import 'package:reduce_image_size/program_info.dart' show name, version;
 
 final projectDirPath = path.dirname(path.dirname(Platform.script.toFilePath()));
-final outputDirPath = path.join(projectDirPath, 'releases/$version');
+final outputDirPath = path.join(projectDirPath, 'build/$version');
 
 final vendorDirPath = path.join(projectDirPath, 'vendor');
 final vendorCopyedDirPath = path.join(outputDirPath, 'vendor');
@@ -13,6 +13,7 @@ final entryPath = path.join(projectDirPath, 'bin/$name.dart');
 final outputBinPath = path.join(outputDirPath, '$name.exe');
 
 Future<void> main(List<String> args) async {
+  // 应该先清空或删除 outputDir
   await Directory(outputDirPath).create(recursive: true);
   await Process.run('dart.exe', ['compile', 'exe', entryPath, '-o', outputBinPath]);
   await copyPath(vendorDirPath, vendorCopyedDirPath);
