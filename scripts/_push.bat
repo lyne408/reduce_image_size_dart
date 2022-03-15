963f0350acce0d21df5f8d99cc683f303d886c69
lyne408 <lyne408@outlook.com>
2022-03-15 13:29:01 +0800

    directory tree

---
+++ scripts/_push.bat
@@ -0,0 +1,5 @@
+git add *
+
+git commit -m "directory tree"
+
+git push origin mian
---
+++ scripts/build_main.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:io/io.dart' show copyPath;
+import 'package:path/path.dart' as path;
+import 'package:reduce_image_size/program_info.dart' show name, version;
+
+final projectDirPath = path.dirname(path.dirname(Platform.script.toFilePath()));
+final outputDirPath = path.join(projectDirPath, 'releases/$version');
+
+final vendorDirPath = path.join(projectDirPath, 'vendor');
+final vendorCopyedDirPath = path.join(outputDirPath, 'vendor');
+
+final entryPath = path.join(projectDirPath, 'bin/$name.dart');
+final outputBinPath = path.join(outputDirPath, '$name.exe');
+
+Future<void> main(List<String> args) async {
+  await Directory(outputDirPath).create(recursive: true);
+  await Process.run('dart.exe', ['compile', 'exe', entryPath, '-o', outputBinPath]);
+  await copyPath(vendorDirPath, vendorCopyedDirPath);
+  print('[Info] Build finished. Check directory:');
+  print('\t$outputDirPath');
+}
